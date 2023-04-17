63a51514a1fe72cfa38835bd67d62c4da7f98649
Becky139 <rebecca.marriott@outlook.com>
2023-04-17 13:33:05 +0000

    add hypothesis to project page

--- app_pages/project_hypothesis.py
+++ app_pages/project_hypothesis.py
@@ -7,10 +7,10 @@ def page_project_hypothesis_body():
 
     # conclusions taken from "" notebook
     st.success(
-        f"* "
+        f"*We beliveve that by looking at differant features we can differentiate between benign or malignant tumours. "
         f"\n\n"
 
-        f"* "
-        f" "
-        f""
+        f"*This analysis aims to observe which features are most helpful in predicting malignant or benign cancer and to see general trends that may aid us in model selection and hyper  parameter selection.\n "
+        f" The goal is to classify whether the breast cancer is benign or malignant\n. "
+        f"To achieve this i have used machine learning classification methods to fit a function that can predict the discrete class of new input.\n"
     )
