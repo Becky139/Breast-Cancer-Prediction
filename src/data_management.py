8e39ae44206866ca6af5ab930652c41d27deb15d
Becky139 <>
2023-05-01 18:45:13 +0000

    work on the data anaylsis streamlit page

--- src/data_management.py
+++ src/data_management.py
@@ -3,7 +3,8 @@ import pandas as pd
 import numpy as np
 import joblib
 
-@st.cache(suppress_st_warning=True, allow_output_mutation=True)
+st.cache(suppress_st_warning=True, allow_output_mutation=True)
+st.set_option('deprecation.showPyplotGlobalUse', False)
 def load_data():
     df = pd.read_csv("outputs/datasets/collection/data.csv")
     return df
