ce99f99a2f667b481ce928f54ee555d3d93020e6
Becky139 <>
2023-06-11 16:02:16 +0000

    fix errors on summarya page

--- app_pages/page_summary.py
+++ app_pages/page_summary.py
@@ -6,16 +6,30 @@ def page_summary_body():
     st.markdown("# Project Summary 🖥️")
     st.sidebar.markdown("# Project Summary 🖥️")
 
-    st.title("Using Predictive Analysis To Predict Diagnosis of a Breast Tumor")
+    st.title(
+        "Using Predictive Analysis To Predict Diagnosis of a Breast Tumor"
+        )
 
     st.info("##  Identify the problem")
     st.write(
-        "Breast cancer is the most common malignancy among women, accounting for nearly 1 in 3 cancers diagnosed among women in the United States, and it is the second leading cause of cancer death among women. Breast Cancer occurs as a results of abnormal growth of cells in the breast tissue, commonly referred to as a Tumor. A tumor does not mean cancer - tumors can be benign (not cancerous), pre-malignant (pre-cancerous), or malignant (cancerous). Tests such as MRI, mammogram, ultrasound and biopsy are commonly used to diagnose breast cancer performed."
+        "Breast cancer is the most common malignancy among women, accounting \
+        for nearly 1 in 3 cancers diagnosed among women in the United States, \
+        and it is the second leading cause of cancer death among women. Breast\
+        Cancer occurs as a results of abnormal growth of cells in the breast \
+        tissue, commonly referred to as a Tumor. A tumor does not mean cancer \
+        - tumors can be benign (not cancerous), pre-malignant (pre-cancerous),\
+        or malignant (cancerous). Tests such as MRI, mammogram, ultrasound and\
+        biopsy are commonly used to diagnose breast cancer performed."
     )
 
     st.info("## Expected outcome")
     st.write(
-        "Given breast cancer results from breast fine needle aspiration (FNA) test (is a quick and simple procedure to perform, which removes some fluid or cells from a breast lesion or cyst (a lump, sore or swelling) with a fine needle similar to a blood sample needle). Since this build a model that can classify a breast cancer tumor using two training classification:"
+        "Given breast cancer results from breast fine needle aspiration (FNA)\
+        test (is a quick and simple procedure to perform, which removes some\
+        fluid or cells from a breast lesion or cyst (a lump, sore or swelling)\
+        with a fine needle similar to a blood sample needle). Since this build\
+        a model that can classify a breast cancer tumor using two training\
+        classification:"
     )
 
     st.info("* 1= Malignant (Cancerous) - Present")
@@ -23,35 +37,56 @@ def page_summary_body():
 
     st.info("### Objective")
     st.write(
-        "Since the labels in the data are discrete, the predication falls into two categories, (i.e. Malignant or benign). In machine learning this is a classification problem."
+        "Since the labels in the data are discrete, the predication falls into\
+        two categories, (i.e. Malignant or benign). In machine learning this\
+        is a classification problem."
     )
 
     st.write(
-        "> *Thus, the goal is to classify whether the breast cancer is benign or malignant and predict the recurrence and non-recurrence of malignant cases after a certain period.  To achieve this we have used machine learning classification methods to fit a function that can predict the discrete class of new input.*"
+        "> *Thus, the goal is to classify whether the breast cancer is benign\
+        or malignant and predict the recurrence and non-recurrence of\
+        malignant cases after a certain period.  To achieve this we have used\
+        machine learning classification methods to fit a function that can\
+        predict the discrete class of new input.*"
     )
 
     # Link to README file, so the users can have access to full project
     # documentation
     st.write(
-        f"* For additional information, please visit and **readme** the "
-        f"[Project README file](https://github.com/Becky139/Breast-Cancer-Prediction/README.md)."
-    )
+        f"* For additional information (particularly regarding the dataset "
+        f"and data preparation), please visit the [Project README file]"
+        f"(https://github.com/Becky139/Breast-Cancer-Prediction/blob/main/"
+        f"/README.md).")
 
     # copied from README file - "Business Requirements" section
     st.success(
-        f"The project has 2 business requirements:\n"
-        f"* 1 - The client is interested conducting a study to differentiate weather a tumor is benign or malignant.\n"
-        f"* As a client, I can navigate easily around an interactive dashboard so that I can view and understand the data presented and I can view and toggle visual graphs, charts of the data.\n\n"
-        f"* 2 - As a client, I can access and see the proccess used in developing the machince learning model with access given to the finalized model.\n"
-        f"* The client is interested in predicting whether a given tumour is malignant or benign based on the given features, with a high degree of accuracy.\n"
+        "The project has 2 business requirements:\n"
+        "* 1 - The client is interested conducting a study to differentiate\
+        weather a tumor is benign or malignant.\n"
+        "* As a client, I can navigate easily around an interactive dashboard\
+        so that I can view and understand the data presented and I can view\
+        and toggle visual graphs, charts of the data.\n\n"
+        "* 2 - As a client, I can access and see the proccess used in\
+        developing the machince learning model with access given to the\
+        finalized model.\n"
+        "* The client is interested in predicting whether a given tumour is\
+        malignant or benign based on the given features, with a high degree\
+        of accuracy.\n"
     )
 
     st.info("# Dataset Content")
 
     st.markdown(
-        "* The dataset is on kaggel it is publicly available so it does not require a license to use. Each row represents a person and the columns have differant signs or textures of tumours. The dataset contains ** 569 samples of malignant and benign tumor cells**.\n"
-        "* The first two columns in the dataset store the unique ID numbers of the samples and the corresponding diagnosis(M=malignant, B=benign), respectively.\n"
-        "* The columns 3-32 contain 30 real-value features that have been computed from digitized images of the cell nuclei, which can be used to build a model to predict whether a tumor is benign or malignant.\n"
+        "* The dataset is on kaggel it is publicly available so it does not\
+        require a license to use. Each row represents a person and the columns\
+        have differant signs or textures of tumours. The dataset contains **\
+        569 samples of malignant and benign tumor cells**.\n"
+        "* The first two columns in the dataset store the unique ID numbers of\
+        the samples and the corresponding diagnosis(M=malignant, B=benign),\
+        respectively.\n"
+        "* The columns 3-32 contain 30 real-value features that have been\
+        computed from digitized images of the cell nuclei, which can be used\
+        to build a model to predict whether a tumor is benign or malignant.\n"
     )
 
     # load data
--- src/data_management.py
+++ src/data_management.py
@@ -1,7 +1,4 @@
-import streamlit as st
 import pandas as pd
-import numpy as np
-import joblib
 
 
 def load_data():
@@ -10,7 +7,8 @@ def load_data():
 
 
 def binary_data():
-    data = pd.read_csv("outputs/datasets/cleaned/binary_data.csv", index_col=False)
+    data = pd.read_csv(
+        "outputs/datasets/cleaned/binary_data.csv", index_col=False)
     data.drop("Unnamed: 0", axis=1, inplace=True)
     return data
 
@@ -18,7 +16,3 @@ def binary_data():
 def raw_data():
     data = pd.read_csv("inputs/datasets/raw/data.csv", index_col=False)
     return data
-
-
-def load_pkl_file(file_path):
-    return joblib.load(filename=file_path)
