485e7c470f64d5bbb14d79603991d159821ba814
Becky139 <>
2023-06-14 07:33:14 +0000

    add. test videos of streamlit tests

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from app_pages.multipage import MultiPage
 from app_pages.page_summary import page_summary_body
 from app_pages.page_project_hypothesis import page_project_hypothesis_body
-from app_pages.page_data_anaylsis import page_data_anaylsis_body
+from app_pages.page_data_anaylsis import page_data_analysis_body
 from app_pages.page_predictive_model import page_predictive_model_body
 from app_pages.page_model_comparison import page_model_comparison_body
 
@@ -13,7 +13,7 @@ app = MultiPage(app_name="Breast Cancer Prediction")
 
 # load pages scripts
 app.add_page("Project Summary", page_summary_body)
-app.add_page("Data Anaylsis", page_data_anaylsis_body)
+app.add_page("Data Anaylsis", page_data_analysis_body)
 app.add_page("Project Hypothesis", page_project_hypothesis_body)
 app.add_page("Predictive Model", page_predictive_model_body)
 app.add_page("Model Comparison", page_model_comparison_body)
